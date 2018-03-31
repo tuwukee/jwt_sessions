bc63c2144bba6657199d480f837a6840234d2f1f
Yulia Oletskaya <yulia.oletskaya@gmail.com>
2018-04-01 00:10:48 +0300

    Check payload content within tests

--- test/units/jwt_sessions/test_session.rb
+++ test/units/jwt_sessions/test_session.rb
@@ -4,22 +4,27 @@ require 'minitest/autorun'
 require 'jwt_sessions'
 
 class TestSession < Minitest::Test
-  attr_accessor :session
+  attr_accessor :session, :payload
   EXPECTED_KEYS = [:access, :csrf, :refresh].freeze
 
   def setup
     JWTSessions.encryption_key = '65994c7b523a3232e7aba54d8cbf'
-    @session = JWTSessions::Session.new
+    @payload = { test: 'test' }
+    @session = JWTSessions::Session.new(payload)
   end
 
   def test_initialization
     token_h = session.login
+    decoded_access = JWTSessions::Token.decode(token_h[:access]).first
     assert_equal EXPECTED_KEYS, token_h.keys.sort
+    assert_equal payload[:test], decoded_access['test']
   end
 
   def test_refresh
     token_h = session.login
-    result = session.refresh(token_h[:refresh])
-    assert_equal EXPECTED_KEYS, result.keys.sort
+    refreshed_token_h = session.refresh(token_h[:refresh])
+    decoded_access = JWTSessions::Token.decode(refreshed_token_h[:access]).first
+    assert_equal EXPECTED_KEYS, refreshed_token_h.keys.sort
+    assert_equal payload[:test], decoded_access['test']
   end
 end
